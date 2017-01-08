3346de03ffea8fc731f2b8f3647fdfbe2c9265f7
Mike James <mike.james@dtcc.edu>
2017-01-07 20:23:35 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+# This program says hello and asks for my name.
+# 
+# exercise from:
+#    "Automating The Boring Stuff With Python:
+#    Practical Programming For Total Beginners"
+#    by Al Sweigart
+#
+
+print('Hello World!')
+
+print('What is your name?') # ask for their name.
+
+myName = input()
+
+print('It is good to meet you, ' + myName)
+
+print('The length of your name is:')
+
+print(len(myName))
+
+print('What is your age?') # ask for their age
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
